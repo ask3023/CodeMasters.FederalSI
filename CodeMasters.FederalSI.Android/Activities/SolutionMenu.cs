709fffc79091b39890b55b104a590ba096f9f138
nullArguments <developer@codestalk.com>
2016-04-02 19:56:48 -0400

    Contact Card

--- CodeMasters.FederalSI.Android/Activities/SolutionMenu.cs
+++ CodeMasters.FederalSI.Android/Activities/SolutionMenu.cs
@@ -13,16 +13,30 @@ using SatelliteMenu;
 using System.Net;
 using Android.Webkit;
 using Android.Support.V7.Widget;
+using CodeMasters.FederalSI.Shared.Service;
+using CodeMasters.FederalSI.Shared.Model;
+using CodeMasters.FederalSI.Droid.Adapters;
 
 namespace CodeMasters.FederalSI.Droid.Activities
 {
     [Activity(Label = "Dot Agile", MainLauncher = true, Icon = "@drawable/loadinganimated", Theme = "@style/FederalSITheme")]
     class SolutionMenu : Activity
     {
+
+        RecyclerView mRecyclerView;
+        RecyclerView.LayoutManager mLayoutManager;
+        
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.SolutionMenu);
+
+            string solutionJson = Intent.GetStringExtra("JsonSelectedSolutionString");
+            var currentSolution = JsonHelper.Deserialize<Solution>(solutionJson);
+
+
+            //recycler
+
 
             var button = FindViewById<SatelliteMenuButton>(Resource.Id.menu);
 
@@ -46,29 +60,56 @@ namespace CodeMasters.FederalSI.Droid.Activities
          
             var imageView = FindViewById<ImageView>(Resource.Id.dynamicImage1);
             var webview = FindViewById<WebView>(Resource.Id.webView1);
-            var cardview = FindViewById<CardView>(Resource.Id.card_view);
+            //var cardview = FindViewById<CardView>(Resource.Id.card_view);
 
             webview.Visibility = ViewStates.Invisible;
             imageView.Visibility = ViewStates.Invisible;
-            cardview.Visibility = ViewStates.Invisible;
+            //cardview.Visibility = ViewStates.Invisible;
 
 
             if (e.MenuItem.Tag == (int)MenuItem.Contact)
             {
                 //set visibility
-                cardview.Visibility = ViewStates.Visible;
+                //cardview.Visibility = ViewStates.Visible;
 
-                var person_photo = FindViewById<ImageView>(Resource.Id.person_photo);
-                person_photo.SetImageResource(Resource.Drawable.BrianBreit);
+                //var person_photo = FindViewById<ImageView>(Resource.Id.person_photo);
+                //person_photo.SetImageResource(Resource.Drawable.BrianBreit);
 
-                var person_name = FindViewById<TextView>(Resource.Id.person_name);
-                person_name.Text = "Brian Breit";
+                //var person_name = FindViewById<TextView>(Resource.Id.person_name);
+                //person_name.Text = "Brian Breit";
 
-                var person_email = FindViewById<TextView>(Resource.Id.person_email);
-                person_email.Text = @"bbreit@deloitte.com";
+                //var person_email = FindViewById<TextView>(Resource.Id.person_email);
+                //person_email.Text = @"bbreit@deloitte.com";
 
-                var person_description = FindViewById<TextView>(Resource.Id.person_description);
-                person_description.Text = "Leads the Federal System Development capability, with 20+ years of experience focusing on large technology-enabled business transformations across Financial Services and Health-related clients in Federal, State, and Commercial.";
+                //var person_description = FindViewById<TextView>(Resource.Id.person_description);
+                //person_description.Text = "Leads the Federal System Development capability, with 20+ years of experience focusing on large technology-enabled business transformations across Financial Services and Health-related clients in Federal, State, and Commercial.";
+
+                string solutionJson = Intent.GetStringExtra("JsonSelectedSolutionString");
+                var currentSolution = JsonHelper.Deserialize<Solution>(solutionJson);
+
+                SetContentView(Resource.Layout.Recycler);
+                mRecyclerView = FindViewById<RecyclerView>(Resource.Id.recyclerView);
+
+                // Plug in the linear layout manager:
+                mLayoutManager = new LinearLayoutManager(this);
+                mRecyclerView.SetLayoutManager(mLayoutManager);
+
+                var dotAgile = new Solution();
+
+                dotAgile.Contacts.Add(new Pointofcontact { Name = "Brian Breit", Email = @"bbreit@deloitte.com", Designation = "Partner",
+                    Description = "Leads the Federal System Development capability, with 20+ years of experience focusing on large technology-enabled business transformations across Financial Services and Health-related clients in Federal, State, and Commercial",
+                    ImageURL= "BrianBreit" });
+                dotAgile.Contacts.Add(new Pointofcontact { Name = "Azunna Anyanwu", Email = @"aanyanwu@deloitte.com", Designation = "Manager",
+                    Description = "15+ years of experience delivering solutions to clients in Federal & State. Expert at applying Agile frameworks such as Scrum & Rapid Application Development (RAD). Certified Scrum Master (CSM) and PMI Agile Professional (PMI-ACP).",
+                    ImageURL = "AzunnaAnyanwu" });
+                dotAgile.Contacts.Add(new Pointofcontact { Name = "Jason Bowers", Email = @"Jbowers@deloitte.com", Designation = "Director",
+                    Description = "Agile advocate with 15+ years in large-scale custom development. Leader in Agile practices to deliver across multiple technology platforms. Delivers solutions in Case Management and other collaborative, mission IT solutions.",
+                    ImageURL = "JasonBowers" });
+
+
+                // Plug in my adapter:
+                var mAdapter = new ContactsAdapter(dotAgile.Contacts);
+                mRecyclerView.SetAdapter(mAdapter);
 
             }
 
---
+++ CodeMasters.FederalSI.Android/Adapters/ContactsAdapter.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using CodeMasters.FederalSI.Shared.Model;
+using Android.Support.V7.Widget;
+
+namespace CodeMasters.FederalSI.Droid.Adapters
+{
+    public class ContactsAdapter : RecyclerView.Adapter
+    {
+        public List<Pointofcontact> Contacts;
+        
+
+        public ContactsAdapter(List<Pointofcontact> contacts)
+        {
+            Contacts = contacts;
+             
+        }
+
+        public override int ItemCount
+        {
+            get { return Contacts.Count; }
+        }
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            ContactViewHolder vh = holder as ContactViewHolder;
+
+            //remove hard coding and add dynamic path
+            //vh.personPhoto.SetImageURI(Android.Net.Uri.Parse("android.resource://SIMarketplace.Android/drawable/" + Contacts[position].ImageURL));
+            if (Contacts[position].Name.Contains("Brian"))
+            {
+                vh.personPhoto.SetImageResource(Resource.Drawable.BrianBreit);
+            }
+            else if (Contacts[position].Name.Contains("Azunna"))
+            {
+                vh.personPhoto.SetImageResource(Resource.Drawable.AzunnaAnyanwu);
+            }
+            else if (Contacts[position].Name.Contains("Jason"))
+            {
+                vh.personPhoto.SetImageResource(Resource.Drawable.JasonBowers);
+            }
+            
+            vh.personName.Text = Contacts[position].Name;
+            vh.personEmail.Text = Contacts[position].Email;
+            vh.personDescription.Text = Contacts[position].Description;
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            // Inflate the CardView for the photo:
+            View itemView = LayoutInflater.From(parent.Context).
+                        Inflate(Resource.Layout.ContactCard, parent, false);
+
+            // Create a ViewHolder to hold view references inside the CardView:
+            ContactViewHolder vh = new ContactViewHolder(itemView);
+            return vh;
+        }
+    }
+
+    public class ContactViewHolder : RecyclerView.ViewHolder
+    {
+        public CardView cv;
+        public TextView personName;
+        public TextView personEmail;
+        public TextView personDescription;
+        public ImageView personPhoto;
+
+        //public ImageView Image { get; private set; }
+        //public TextView Caption { get; private set; }
+
+        public ContactViewHolder(View itemView) : base(itemView)
+        {
+            // Locate and cache view references:
+            cv = itemView.FindViewById<CardView>(Resource.Id.card_view);
+            personName = itemView.FindViewById<TextView>(Resource.Id.person_name);
+            personEmail = itemView.FindViewById<TextView>(Resource.Id.person_email);
+            personPhoto = itemView.FindViewById<ImageView>(Resource.Id.person_photo);
+            personDescription = itemView.FindViewById<TextView>(Resource.Id.person_description);
+        }
+    }
+}
